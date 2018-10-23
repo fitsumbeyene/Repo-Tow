021d12cbb2d906433183bf504c612d511c2f6ceb
fitsumbeyene <beyeneaddis@yahoo.com>
2018-10-23 16:34:57 -0700

    new

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 'use string'
 let people = prompt('Do you think Womens Population is higher than Mens?')
 if(people.toLocaleLowerCase() === 'yes'){
-   alert('Right, Womens Population is higher than Mens.')
+   alert('Right, git Womens Population is higher than Mens.')
 }
 else 
 alert("Wrong, the answer is yes.")
+
 
 
 //let person = prompt("do you think a person can live one month without wateer?")
