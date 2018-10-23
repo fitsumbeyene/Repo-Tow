378bb9df723fd1afdcf2c6e3262f7b906ccb9ede
fitsumbeyene <beyeneaddis@yahoo.com>
2018-10-23 16:16:10 -0700

    my first update of  lab-2

---
+++ main.js
@@ -0,0 +1,20 @@
+'use string'
+let people = prompt('Do you think Womens Population is higher than Mens?')
+if(people.toLocaleLowerCase() === 'yes'){
+   alert('Right, Womens Population is higher than Mens.')
+}
+else 
+alert("Wrong, the answer is yes.")
+
+
+//let person = prompt("do you think a person can live one month without wateer?")
+
+
+
+//let Ethiopi = promt("?")
+
+
+
+
+
+
