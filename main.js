43f754a79baab96bc63f443e5a38d63c0e809854
fitsumbeyene <beyeneaddis@yahoo.com>
2018-10-24 05:50:05 -0700

    my first pro

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
 'use string'
-let people = prompt('Do you think Womens Population is higher than Mens?')
+let people = prompt('Do you think Womens Population are higher than Mens?')
 if(people.toLocaleLowerCase() === 'yes'){
-   alert('Right, git Womens Population is higher than Mens.')
+   alert('right, Womens Population is higher than Mens.')
 }
 else 
 alert("Wrong, the answer is yes.")
 
+let person = prompt(" man can live a month without water?")
+if(person.toLocaleLowerCase() === 'true'){
+  alert("false. a person can not live a month without water.")}
+  else
+  alert("true, a person can not live a month without water.")
+
+  let Pig = prompt("they said, Pig is the smartest animal in the world?.")
+  if(Pig.toLocaleLowerCase() === 'yes'){
+    alert("right, its good to know yourself.")}
+    else
+alert("wrong, You don't know yourself!.")
+
+  let  guy = peomt("can you touch your right ear with your left hand over your head.")
+  if(guy.toLocaleLowerCase() === 'yes'){
+    alerst("correct, you are full.")
+  }
+    else
+    alert("incorrect, dont afraid to try!.")
+
+ let animal = promt("Cheter is the fastest animal in the world?.")
+ if(animal.toLocaleLowerCase() === 'True'){
+   alert("true, butt he dosen't know JS.")}
+else
+alert("false, but he is good at html,css,and js.")
+ 
+  
 
 
-//let person = prompt("do you think a person can live one month without wateer?")
 
 
 
-//let Ethiopi = promt("?")
+
+
+
+
+
 
 
 
