1f8686f12f475db29f0c01bdffa956112f92c4c9
fitsumbeyene <beyeneaddis@yahoo.com>
2018-10-25 20:43:29 -0700

    this is entro

--- main.js
+++ main.js
@@ -1,50 +1,80 @@
-'use string'
-let people = prompt('Do you think Womens Population are higher than Mens?')
-if(people.toLocaleLowerCase() === 'yes'){
-   alert('right, Womens Population is higher than Mens.')
-}
-else 
-alert("Wrong, the answer is yes.")
-
-let person = prompt(" man can live a month without water?")
-if(person.toLocaleLowerCase() === 'true'){
-  alert("false. a person can not live a month without water.")}
-  else
-  alert("true, a person can not live a month without water.")
-
-  let Pig = prompt("they said, Pig is the smartest animal in the world?.")
-  if(Pig.toLocaleLowerCase() === 'yes'){
-    alert("right, its good to know yourself.")}
-    else
-alert("wrong, You don't know yourself!.")
-
-  let  guy = peomt("can you touch your right ear with your left hand over your head.")
-  if(guy.toLocaleLowerCase() === 'yes'){
-    alerst("correct, you are full.")
-  }
-    else
-    alert("incorrect, dont afraid to try!.")
-
- let animal = promt("Cheter is the fastest animal in the world?.")
- if(animal.toLocaleLowerCase() === 'True'){
-   alert("true, butt he dosen't know JS.")}
-else
-alert("false, but he is good at html,css,and js.")
- 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//  'use string'
+//  let people = prompt('Do you think Womens Population are higher than Mens?')
+// if(people.toLocaleLowerCase() === 'yes' ||
+// people.toLocaleUpperCase() === 'YES' ||
+//  people.toLocaleLowerCase() === 'y' ||
+//  people.toLocaleUpperCase() === 'Y'){
+//   alert(people + ', acordind to the statistics Womens Population is more higher than Mens.')
+//   counter++
+//  }else {
+//   alert(people  + ', Wrong, the answer is yes.')
+// }
+// let person = prompt(" can a man live a month without   water?")
+// if(person.toLocaleLowerCase() === 'yes' || 
+// person.toLocaleUpperCase() === 'YES' ||
+// person.toLocaleLowerCase() === 'y' ||
+// person.toLocaleUpperCase() === 'Y' ){
+//   alert(person + 'that is correct. a person can not live without water.')
+//   counter++
+// } 
+//   else{
+//   alert(person + ',that is iccorrect, a person can not live without water.')
+// }
+// //  console.log(oerson + ', can not live without water.')
+// let Pig = prompt("they said, Pig is the smartest animal in the world?.")
+// if(Pig.toLocaleLowerCase() === 'yes' ||
+// pig.toLocaleUpperCase() === 'YES' ||
+// pig.toLocaleLowerCase() === 'y' ||
+// pig.toLocaleUpperCase() === 'Y' ){
+// alert(pig, + ', is correct, and its good to know yourself.')
+// counter++
+// }
+// else{
+// alert(pig + ', that is not correct. You don\' know yourself!.')
+// let guy = peomt("can you touch your right ear with your left hand over your head?")
+//  if(guy.toLocaleLowerCase() === 'yes' ||
+//  guy.toLocaleUpperCase() === 'YES' ||
+//  guy.toLocaleLowerCase() === 'y' ||
+//  guy.toLocaleUpperCase() === 'Y' ){
+//   alerst(guy + ", is the correct, you are full.")
+//   counter++
+//   }
+// else{
+//   alert(guy + ',is incorrect, dont afraid to try!.')
+// let animal = promt ("is Cheter the fastest animal in the world?.")
+//  if(animal.toLocaleLowerCase() === 'yes' ||
+//  animal.toLocaleUpperCase() === 'YES' ||
+//  animal.toLocaleLowerCase() === 'y' ||
+//  animal.toLocaleUpperCase() === 'Y' ){
+// alert(animal + ', is the fastest animal in the world . butt he dosen\'t know JS.')
+// counter++
+//  }
+//   else{
+//  alert(animal + ', that is not correct. cheter is the fastest animal in the world.')
+// let newjob = confirm ('do you think by the end of this year, you will be in the job market?')
+//   if (newjob) {
+//    alert('Greet! but you have to study very hard.')
+//    counter++
+// }else {
+//   alert('don\'t worry. its not end of the world. one day is one day.')
+// }
+//   let internationalArray = ['lion','leopard','buffalo','elephanat']
+// for(let i = 0; i < internationalArray.length; i++){
+// let international = prompt('what are the bigest known animal')
+//  if((internationalArray.length -1) - (i) === 0){
+//    alert('sorry, you are incorrect. next question.')
+// }
+//  else if(international  === '4'){
+//  alert('good job! the big four animals ' + internationalArray.length + 'animals')
+//  counter++
+// }
+//  else if(international < '4'){
+//    alert('sorry.that\'s too few. you have ' + ((internationlaArray.length -1) - (i)) + ' more chanses.')
+//  }
+//     else if (international.lebgth > '4'){
+//  alert('sorry, that\'s too many. you have '+ (
+//    ( internationalArray.length -1) - (i)) + 'more chances.')
+//  }
+//  console.log('Index is now', i)
+//   console.log(internationalArray[i]) }
+// }
